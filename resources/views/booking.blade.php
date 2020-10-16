4c4be895b87f9d05189e38b6719be2c270d4fb21
Arslan5454 <arslanhanif5454@gmail.com>
2020-10-16 00:27:23 -0700

    replace time

--- resources/views/booking.blade.php
+++ resources/views/booking.blade.php
@@ -3,7 +3,7 @@
 <body >
 <!-- style="background-image: url('img/restaurant/1.jpg');" -->
 <div class="header_booking">
-<h1>Booking Table</h1>
+<h1>Online Booking Table</h1>
 </div>
 <br>
 @if(session('status'))
@@ -19,63 +19,13 @@
 
         @csrf
         <label>Select Date:</label>&nbsp;&nbsp;
-        <i class="fa fa-calendar">&nbsp;&nbsp;&nbsp;<input type="date" size="30" name="date" /></i>
+        <i class="fa fa-calendar">&nbsp;&nbsp;&nbsp;<input type="date" size="30"  class="select" name="date" /></i>
         <br>
         <br>
         <label>Timing:</label>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
         <i class="fa fa-clock-o"></i>
         &nbsp;&nbsp;&nbsp;
-        <select  class="select" name="time">
-            <option value="12:00 AM">12:00 AM</option>
-            <option value="12:30 AM">12:30 AM</option>
-            <option value="1:00 AM">1:00 AM</option>
-            <option value="1:30 AM">1:30 AM</option>
-            <option value="2:00 AM">2:00 AM</option>
-            <option value="2:30 AM">2:30 AM</option>
-            <option value="3:00 AM">3:00 AM</option>
-            <option value="3:30 AM">3:30 AM</option>
-            <option value="4:00 AM">4:00 AM</option>
-            <option value="4:30 AM">4:30 AM</option>
-            <option value="5:00 AM">5:00 AM</option>
-            <option value="5:30 AM">5:30 AM</option>
-            <option value="6:00 AM">6:00 AM</option>
-            <option value="6:30 AM">6:30 AM</option>
-            <option value="7:00 AM">7:00 AM</option>
-            <option value="7:30 AM">7:30 AM</option>
-            <option value="8:00 AM">8:00 AM</option>
-            <option value="8:30 AM">8:30 AM</option>
-            <option value="9:00 AM">9:00 AM</option>
-            <option value="9:30 AM">9:30 AM</option>
-            <option value="10:00 AM">10:00 AM</option>
-            <option value="10:30 AM">10:30 AM</option>
-            <option value="11:00 AM">11:00 AM</option>
-            <option value="11:30 AM">11:30 AM</option>
-            <option value="12:00 PM">12:00 PM</option>
-            <option value="12:30 PM">12:30 PM</option>
-            <option value="1:00 PM">1:00 PM</option>
-            <option value="1:30 PM">1:30 PM</option>
-            <option value="2:00 PM">2:00 PM</option>
-            <option value="2:30 PM">2:30 PM</option>
-            <option value="3:00 PM">3:00 PM</option>
-            <option value="3:30 PM">3:30 PM</option>
-            <option value="4:00 PM">4:00 PM</option>
-            <option value="4:30 PM">4:30 PM</option>
-            <option value="5:00 PM">5:00 PM</option>
-            <option value="5:30 PM">5:30 PM</option>
-            <option selected="selected" value="6:00 PM">6:00 PM</option>
-            <option value="6:30 PM">6:30 PM</option>
-            <option value="7:00 PM">7:00 PM</option>
-            <option value="7:30 PM">7:30 PM</option>
-            <option value="8:00 PM">8:00 PM</option>
-            <option value="8:30 PM">8:30 PM</option>
-            <option value="9:00 PM">9:00 PM</option>
-            <option value="9:30 PM">9:30 PM</option>
-            <option value="10:00 PM">10:00 PM</option>
-            <option value="10:30 PM">10:30 PM</option>
-            <option value="11:00 PM">11:00 PM</option>
-            <option value="11:30 PM">11:30 PM</option>
-        </select>
-        </input>
+        <input type="time" class="select" name="time">
         <br>
         <br>
         <label>Person:</label>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
@@ -129,21 +79,21 @@
                         <label>Standard</label>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
                         &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
                         &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
-                        &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+                        &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
                         <input type="checkbox" id="vehicle1" name="vehicle1" value="Bike">
                         <br>
                         <br>
                         <label>Hight</label>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
                         &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
                         &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
-                        &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+                        &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
                         <input type="checkbox" id="vehicle1" name="vehicle1" value="Bike">
                         <br>
                         <br>
                         <label>Top</label>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
                         &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
                         &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
-                        &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+                        &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
                         <input type="checkbox" id="vehicle1" name="vehicle1" value="Bike">
                     </div>
                     <div class="modal-footer">
